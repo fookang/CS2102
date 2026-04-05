6f6638c0593c7f46f3203de05e4875f81b5f00db
Foo Kang <fookang3@gmail.com>
2026-04-05 23:38:32 +0800

    Add data processing functions to generate SQL insert statements from CSV

---
+++ P03/get_data.py
@@ -0,0 +1,198 @@
+import pandas as pd
+
+OUTPUT_FILE = "data.sql"
+
+
+def as_str(v):
+    """
+    Return the value as a string that can be
+    accepted by postgresql as string.
+    """
+
+    if pd.isna(v):
+        return 'NULL'
+
+    s = f'{v}'.replace("'", "''")
+    return f"'{s}'"
+
+
+def read_csv(file):
+    return pd.read_csv(file)
+
+
+def clean(file):
+    with open(file, 'w') as f:
+        f.write("")
+
+
+def write_region_data(file, data):
+    regions = data[['rider_region', 'start_region', 'finish_region',
+                    'team_region']].stack().drop_duplicates().reset_index(drop=True)
+
+    with open(file, 'a') as f:
+        f.write("-- Insert Regions Data\n")
+        for region in regions:
+            f.write(f"INSERT INTO Region VALUES ('{region}');\n")
+
+        f.write("\n\n")
+
+
+def write_country_data(file, data):
+    countries = pd.concat([
+        data[['rider_country_code', 'rider_country_name', 'rider_region']].rename(columns={
+            'rider_country_code': 'code',
+            'rider_country_name': 'name',
+            'rider_region': 'region'
+        }),
+        data[['start_country_code', 'start_country_name', 'start_region']].rename(columns={
+            'start_country_code': 'code',
+            'start_country_name': 'name',
+            'start_region': 'region'
+        }),
+        data[['finish_country_code', 'finish_country_name', 'finish_region']].rename(columns={
+            'finish_country_code': 'code',
+            'finish_country_name': 'name',
+            'finish_region': 'region'
+        }),
+        data[['team_country_code', 'team_country_name', 'team_region']].rename(columns={
+            'team_country_code': 'code',
+            'team_country_name': 'name',
+            'team_region': 'region'
+        })
+    ]).drop_duplicates().dropna().reset_index(drop=True)
+
+    with open(file, 'a') as f:
+        f.write("-- Insert countries Data\n")
+        for country in countries.itertuples(index=False):
+            f.write(
+                f"INSERT INTO countries VALUES ({as_str(country.code)}, {as_str(country.name)}, {as_str(country.region)});\n")
+
+        f.write("\n\n")
+
+
+def write_team_data(file, data):
+    teams = data[['team', 'team_country_code']
+                 ].drop_duplicates().reset_index(drop=True)
+    with open(file, 'a') as f:
+        f.write("-- Insert teams Data\n")
+        for team in teams.itertuples(index=False):
+            f.write(
+                f"INSERT INTO teams VALUES ({as_str(team.team)}, {as_str(team.team_country_code)});\n")
+
+        f.write("\n\n")
+
+
+def write_rider_data(file, data):
+    riders = data[['bib', 'rider', 'dob', 'team']
+                  ].drop_duplicates().reset_index(drop=True)
+    with open(file, 'a') as f:
+        f.write("-- Insert riders Data\n")
+        for rider in riders.itertuples(index=False):
+            f.write(
+                f"INSERT INTO riders VALUES ({rider.bib}, {as_str(rider.rider)}, {as_str(rider.dob)}, {as_str(rider.team)});\n")
+
+        f.write("\n\n")
+
+
+def write_rider_from_data(file, data):
+    rider_from = data[['bib', 'rider_country_code']].drop_duplicates().dropna(
+        subset=['rider_country_code']).reset_index(drop=True)
+    with open(file, 'a') as f:
+        f.write("-- Insert riders_from Data\n")
+        for rf in rider_from.itertuples(index=False):
+            f.write(
+                f"INSERT INTO riders_from VALUES ({rf.bib}, {as_str(rf.rider_country_code)});\n")
+
+        f.write("\n\n")
+
+
+def write_member_of_data(file, data):
+    member_of = data[['bib', 'team']].drop_duplicates().reset_index(drop=True)
+    with open(file, 'a') as f:
+        f.write("-- Insert Member_Of Data\n")
+        for mo in member_of.itertuples(index=False):
+            f.write(
+                f"INSERT INTO Member_Of VALUES ({mo.bib}, {as_str(mo.team)});\n")
+
+        f.write("\n\n")
+
+
+def write_location_data(file, data):
+    locations = pd.concat([
+        data[['start_location', 'start_country_code']].rename(
+            columns={'start_location': 'location', 'start_country_code': 'country'}),
+        data[['finish_location', 'finish_country_code']].rename(
+            columns={'finish_location': 'location', 'finish_country_code': 'country'})
+    ]).drop_duplicates().reset_index(drop=True)
+
+    with open(file, 'a') as f:
+        f.write("-- Insert locations Data\n")
+        for location in locations.itertuples(index=False):
+            f.write(
+                f"INSERT INTO locations VALUES ({as_str(location.location)}, {as_str(location.country)});\n")
+
+        f.write("\n\n")
+
+
+def write_stage_type(file, data):
+    types = data['type'].drop_duplicates().reset_index(drop=True)
+    with open(file, 'a') as f:
+        f.write("-- Insert Stage Types Data\n")
+        for t in types:
+            f.write(f"INSERT INTO Stage_Type VALUES ('{t}');\n")
+        f.write("\n\n")
+
+
+def write_stage_data(file, data):
+    stages = data[['stage', 'day', 'start_location', 'finish_location', 'length', 'type'
+                   ]].drop_duplicates().reset_index(drop=True)
+
+    with open(file, 'a') as f:
+        f.write("-- Insert stages Data\n")
+        for stage in stages.itertuples(index=False):
+            f.write(
+                f"INSERT INTO stages VALUES ({stage.stage}, {as_str(stage.day)}, {as_str(stage.start_location)}, {as_str(stage.finish_location)}, {stage.length}, {as_str(stage.type)});\n")
+
+        f.write("\n\n")
+
+
+def write_riders_results_data(file, data):
+    results = data[['stage', 'bib', 'time', 'bonus', 'penalty', 'rank'
+                    ]].drop_duplicates().reset_index(drop=True)
+
+    with open(file, 'a') as f:
+        f.write("-- Insert Riders_Results Data\n")
+        for result in results.itertuples(index=False):
+            f.write(
+                f"INSERT INTO Result VALUES ({result.stage}, {result.bib}, {as_str(result.time)}, {result.bonus}, {result.penalty}, {result.rank});\n")
+        f.write("\n\n")
+
+
+def write_riders_exits_data(file, data_exit, data):
+    processed = data_exit[['bib', 'stage', 'reason'
+                           ]].drop_duplicates().reset_index(drop=True)
+
+    dropped = processed[~processed['bib'].isin(data['bib'])]
+    processed = processed[processed['bib'].isin(data['bib'])]
+    print("Dropped bibs:", dropped['bib'].tolist())
+
+    with open(file, 'a') as f:
+        f.write("-- Insert riders_exits Data\n")
+        for exit in processed.itertuples(index=False):
+            f.write(
+                f"INSERT INTO riders_exits VALUES ({exit.bib}, {exit.stage}, {as_str(exit.reason)});\n")
+        f.write("\n\n")
+
+
+if __name__ == "__main__":
+    data = read_csv('tdf-2025.csv')
+    data_exit = read_csv('tdf-exits.csv')
+
+    clean(OUTPUT_FILE)
+    write_country_data(OUTPUT_FILE, data)
+    write_team_data(OUTPUT_FILE, data)
+    write_rider_data(OUTPUT_FILE, data)
+    write_rider_from_data(OUTPUT_FILE, data)
+    write_location_data(OUTPUT_FILE, data)
+    write_stage_data(OUTPUT_FILE, data)
+    write_riders_exits_data(OUTPUT_FILE, data_exit, data)
